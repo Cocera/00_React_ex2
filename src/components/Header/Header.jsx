491a2dd90680d61de0ba917e71de07dd4af62a0f
Cocera <alcocera@gmail.com>
2023-11-20 16:33:26 +0100

    Components => Header & Home

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+// import "./Header.css"
+
+const Header = () => {
+    return <>
+    <h1>Esto es el header</h1>
+    </>
+};
+
+export default Header;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+// import "./Home.css"
+
+const Home = () => {
+    return <>
+    <h1>Esto es el home</h1>
+    </>
+};
+
+export default Home;
