a7b38395d9ee667014ea2c8d2c200e4b3d9848ce
Cocera <alcocera@gmail.com>
2023-11-21 09:24:34 +0100

    ok

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = (props) => {
         <h1>Introduction to {props.title}</h1>
         <p>Descripción de texto. La flor es la estructura reproductiva característica de las plantas llamadas espermatofitas o fanerógamas. La función de una flor es producir semillas a través de la reproducción sexual. Para las plantas, las semillas son la próxima generación y sirven como el principal medio a través del cual las especies se perpetúan y se propagan.</p>
         <div className="img_square">
-            <img className="shadow object-fit-cover border rounded" src={props.link}/>
+            <img className="shadow object-fit-cover border" src={props.link}/>
         </div>
     </div>
 };
